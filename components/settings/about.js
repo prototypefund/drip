bdbe1724f166b0e16de438623011ddc9893b2a82
mashazyu <mariya.z@gmail.com>
2020-03-18 16:25:19 +0100

    About and License pages redesign

--- components/settings/about.js
+++ components/settings/about.js
@@ -1,33 +1,56 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { ScrollView } from 'react-native'
-import AppText from '../app-text'
+import PropTypes from 'prop-types'
+
+import FramedSegment from '../framed-segment'
+import { GreyText } from '../app-text'
+import { Title } from '../app-text'
+
+import { connect } from 'react-redux'
+import { getDimensions } from '../../slices/dimensions'
+
 import labels from '../../i18n/en/settings'
 import links from '../../i18n/en/links'
-import FramedSegment from '../framed-segment'
+import { default as common } from '../../styles/redesign'
 
-export default class AboutSection extends Component {
-  render() {
-    return (
-      <ScrollView>
-        <FramedSegment title={labels.aboutSection.title}>
-          <AppText>{labels.aboutSection.text}</AppText>
-        </FramedSegment>
-        <FramedSegment title={labels.philosophy.title}>
-          <AppText>{labels.philosophy.text}</AppText>
-        </FramedSegment>
-        <FramedSegment title={labels.credits.title}>
-          <AppText>{labels.credits.note}</AppText>
-        </FramedSegment>
-        <FramedSegment title={labels.donate.title}>
-          <AppText>{labels.donate.note}</AppText>
-        </FramedSegment>
-        <FramedSegment title={labels.website.title}>
-          <AppText>{links.website.url}</AppText>
-        </FramedSegment>
-        <FramedSegment title={labels.version.title} last>
-          <AppText>{require('../../package.json').version}</AppText>
-        </FramedSegment>
-      </ScrollView>
-    )
-  }
+const AboutSection = ({ dimensions }) => {
+  const { pageHeight } = dimensions
+  return (
+    <ScrollView style={[common.pageContainer, {height: pageHeight}]}>
+      <Title>{labels.aboutSection.title}</Title>
+      <FramedSegment>
+        <GreyText>{labels.aboutSection.text}</GreyText>
+      </FramedSegment>
+      <FramedSegment title={labels.philosophy.title}>
+        <GreyText>{labels.philosophy.text}</GreyText>
+      </FramedSegment>
+      <FramedSegment title={labels.credits.title}>
+        <GreyText>{labels.credits.note}</GreyText>
+      </FramedSegment>
+      <FramedSegment title={labels.donate.title}>
+        <GreyText>{labels.donate.note}</GreyText>
+      </FramedSegment>
+      <FramedSegment title={labels.website.title}>
+        <GreyText>{links.website.url}</GreyText>
+      </FramedSegment>
+      <FramedSegment title={labels.version.title} last>
+        <GreyText>{require('../../package.json').version}</GreyText>
+      </FramedSegment>
+    </ScrollView>
+  )
 }
+
+AboutSection.propTypes = {
+  dimensions: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => {
+  return({
+    dimensions: getDimensions(state),
+  })
+}
+
+export default connect(
+  mapStateToProps,
+  null,
+)(AboutSection)
--- components/settings/license.js
+++ components/settings/license.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react'
-import { View, ScrollView } from 'react-native'
-import AppText from '../app-text'
-import styles from '../../styles/index'
-import labels from '../../i18n/en/settings'
+import React from 'react'
+import { ScrollView } from 'react-native'
+import PropTypes from 'prop-types'
 
-export default class License extends Component {
-  render() {
-    return (
-      <ScrollView>
-        <View style={styles.framedSegment}>
-          <AppText style={styles.framedSegmentTitle}>{`${labels.license.title} `}</AppText>
-          <AppText>{`${labels.license.text} `}</AppText>
-        </View>
-      </ScrollView>
-    )
-  }
+import { GreyText } from '../app-text'
+import FramedSegment from '../framed-segment'
+import { Title } from '../app-text'
+
+import { connect } from 'react-redux'
+import { getDimensions } from '../../slices/dimensions'
+
+import labels from '../../i18n/en/settings'
+import { default as common } from '../../styles/redesign'
+
+const License = ({ dimensions }) => {
+  const { pageHeight } = dimensions
+  return (
+    <ScrollView style={[common.pageContainer, {height: pageHeight}]}>
+      <Title>{labels.menuTitles.license}</Title>
+      <FramedSegment title={labels.license.title} last>
+        <GreyText>{`${labels.license.text} `}</GreyText>
+      </FramedSegment>
+    </ScrollView>
+  )
 }
+
+License.propTypes = {
+  dimensions: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => {
+  return({
+    dimensions: getDimensions(state),
+  })
+}
+
+export default connect(
+  mapStateToProps,
+  null,
+)(License)
