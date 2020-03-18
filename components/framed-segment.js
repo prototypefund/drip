684d31152f744174c0c3e4c497d5acc345a6c58e
mashazyu <mariya.z@gmail.com>
2020-03-18 16:17:53 +0100

    FramedSegment component redesign

--- components/framed-segment.js
+++ components/framed-segment.js
@@ -3,24 +3,24 @@ import PropTypes from 'prop-types'
 
 import { View } from 'react-native'
 import AppText from './app-text'
-import styles from '../styles'
+import styles from '../styles/redesign'
 
 const FramedSegment = ({ children, last, style, title }) => {
   const viewStyle = [styles.framedSegment, style]
   if (last) viewStyle.push(styles.framedSegmentLast)
   return (
     <View style={[viewStyle]}>
-      {title && <AppText style={styles.framedSegmentTitle}>{title}</AppText>}
+      {title && <AppText style={styles.framedSegmentTitle}>
+        {title}</AppText>
+      }
       {children}
+      {!last && <View style={styles.line}></View>}
     </View>
   )
 }
 
 FramedSegment.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ]),
+  children: PropTypes.node,
   last: PropTypes.bool,
   style: PropTypes.object,
   title: PropTypes.string
